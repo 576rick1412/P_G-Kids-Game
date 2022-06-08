74705f2c8f325c47a9874e4ddfce8bd657b66c46
576rick1412 <576rick1412@gmail.com>
2022-06-08 11:10:07 +0900

    오늘 수업중에 한거 저장
    뷁

---
+++ Assets/CS/Button/Setting_Button.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Setting_Button : MonoBehaviour
+{
+    public void proto_b()
+    {
+        Debug.Log("세팅 눌림");
+    }
+}
